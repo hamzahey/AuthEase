b5f4023c1154f7e8aeefc14401486b26193064d7
hamzahey <hamzakhursheed19@yahoo.com>
2024-07-23 13:54:08 +0500

    Add Enviroment Variables

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ import bcrypt from "bcrypt";
 import session from "express-session";
 import passport from "passport";
 import { Strategy } from "passport-local";
+import env from "dotenv"
 
 const app = express();
 const port = 3002;
 const saltRounds = 10;
+env.config();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.use(session({
-  secret: "TOPSECRETWORD",
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
